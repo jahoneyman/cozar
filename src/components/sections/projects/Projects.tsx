f29a598256c538c2cb2dbc79470ae056013e81d4
John Honeyman <johnhoneyman10@gmail.com>
2026-08-12 16:45:44 +0800

    feat: Initial Project Card component implementation

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -1,3 +1,5 @@
+import { ProjectCard } from "./components";
+
 const Projects = () => {
   return (
     <section id="projects" className="px-6 py-16 bg-neutral-offwhite">
@@ -5,6 +7,14 @@ const Projects = () => {
         <h2 className="text-primary-900 font-display font-semibold text-[32px]">
           Our Projects
         </h2>
+        <div>
+          <ProjectCard
+            image="/images/projects/project_1.png"
+            name="Project Name"
+            type="Construction"
+            location="Location"
+          />
+        </div>
       </div>
     </section>
   );
--- src/components/sections/projects/components/ProjectCard.tsx
+++ src/components/sections/projects/components/ProjectCard.tsx
@@ -1,5 +1,43 @@
-const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProjectCardProps {
+  image: string | StaticImport;
+  name?: string;
+  type?: string;
+  location?: string;
+}
+
+const ProjectCard = ({
+  image,
+  name = "Project Name",
+  type = "Project Type",
+  location = "Location",
+}: ProjectCardProps) => {
+  return (
+    <div className="w-[320px]">
+      <div className="relative h-100 p-6">
+        <h3 className="relative z-10 text-white">{type}</h3>
+        <Image src={image} alt="" fill className="object-cover" />
+        <div
+          aria-hidden
+          className="absolute inset-0 opacity-30 bg-linear-to-b from-primary-1000 to-primary-1000/50 to-50% pointer-events-none"
+        />
+      </div>
+      <div className="p-4 flex justify-between items-start">
+        <div className="flex flex-col gap-2 text-label-md">
+          <h3 className="text-primary-1000">{name}</h3>
+          <h3 className="text-neutral-gray">{location}</h3>
+        </div>
+        <div className="flex flex-col gap-2 text-label-md font-semibold">
+          <Link href={""} className="text-primary-400 border-b-[0.5px] pb-1">
+            View Project
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
